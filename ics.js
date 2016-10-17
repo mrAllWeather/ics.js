8557e14d566abf752aad84327685c153ae2b7b82
Ben Weatherall <ben.weatherall@gmail.com>
2016-10-17 16:24:31 +1030

    Improved 0 Second event check
    Previous version risked incorrectly identifying 11:10-12:09 as an all day event.

--- ics.js
+++ ics.js
@@ -123,7 +123,7 @@ var ics = function() {
             // Since some calendars don't add 0 second events, we need to remove time if there is none...
             var start_time = '';
             var end_time = '';
-            if ((end_hours - start_hours) + (end_minutes - start_minutes) + (end_seconds - start_seconds) !== 0) {
+            if ((end_hours - start_hours) !== 0 || (end_minutes - start_minutes) !== 0 || (end_seconds - start_seconds) !== 0) {
                 start_time = 'T' + start_hours + start_minutes + start_seconds;
                 end_time = 'T' + end_hours + end_minutes + end_seconds;
             }
